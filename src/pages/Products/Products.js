837ee5ba9d882fc8ef307afe7b3550ef261c39e1
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-04-05 23:03:11 +0300

    custom hooks used

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,30 +1,13 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {SafeAreaView, FlatList, ActivityIndicator, Text} from 'react-native';
 import {API_URL} from '@env';
 //import Config from 'react-native-config';
 
 import ProductCard from '../../components/ProductCard';
+import useFetch from '../../hooks/useFetch';
 
 const Products = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const {data: productData} = await axios.get(API_URL);
-      setData(productData);
-      setLoading(false);
-    } catch (err) {
-      setError(err.message);
-      setLoading(false);
-    }
-  };
+  const {loading, data, error} = useFetch(API_URL);
 
   const renderProduct = ({item}) => <ProductCard product={item} />;
 
