4ec13946359b2cc07bfdca6260ce7b32061947eb
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-04-05 22:45:10 +0300

    before custom hook

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, FlatList} from 'react-native';
+import {SafeAreaView, FlatList, ActivityIndicator, Text} from 'react-native';
 import {API_URL} from '@env';
 //import Config from 'react-native-config';
 
 import ProductCard from '../../components/ProductCard';
 
 const Products = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -14,11 +16,25 @@ const Products = () => {
   }, []);
 
   const fetchData = async () => {
-    const {data: productData} = await axios.get(API_URL);
-    setData(productData);
+    try {
+      const {data: productData} = await axios.get(API_URL);
+      setData(productData);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
   };
 
   const renderProduct = ({item}) => <ProductCard product={item} />;
+
+  if (loading) {
+    return <ActivityIndicator size={'large'} />;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
 
   return (
     <SafeAreaView>
